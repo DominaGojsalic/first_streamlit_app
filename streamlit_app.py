2f4e613f9dc9d7c14a57b72f5d3ecaf9ced55399
DominaGojsalic <106979761+DominaGojsalic@users.noreply.github.com>
2022-11-16 16:42:20 +0100

    Update streamlit_app.py
    added breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,9 @@ streamlit.title('Hello')
 
 streamlit.header('Lesson 1')
 streamlit.text('Create Git Repository')
+  
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔  Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞  Avocado Toast')
