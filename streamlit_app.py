5d6f17470ec82d39966f433ea550e49bedda4796
DominaGojsalic <106979761+DominaGojsalic@users.noreply.github.com>
2022-11-17 09:38:15 +0100

    Update streamlit_app.py
    fix import to read csv

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,5 +14,5 @@ streamlit.text('🥑🍞  Avocado Toast')
 
 import pandas
 
-my_fruit_list=pandas.import_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
